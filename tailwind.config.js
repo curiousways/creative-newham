f46ce90b19560bb75e216fb5193fa71ac32c4ed3
Oluwafemi Kayode <mrlapinho@gmail.com>
2022-06-29 09:23:07 +0100

    Configure tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} **/
 
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
@@ -10,6 +12,13 @@ module.exports = {
       screens: {
         "2xl": "1440px",
         "3xl": "1920px",
+      },
+      fontFamily: {
+        sans: ["Mabry", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        "cn-blue": "#402EC1",
+        "cn-orange": "#FFA700",
       },
     },
   },
