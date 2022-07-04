d52227be74593275cfd232de1c50fc6c5bf531f9
Oluwafemi Kayode <mrlapinho@gmail.com>
2022-07-04 13:07:53 +0100

    Update domain and add analytics

--- next.config.js
+++ next.config.js
@@ -3,10 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
   env: {
-    SITE_URL: "site url",
+    SITE_URL: "https://www.creativenewham.com",
     SITE_TITLE: "The Creative Newham Alliance",
-    GOOGLE_ANALYTICS_ID: "yourgaid",
-    FATHOM_ANALYTICS_ID: "yourfathomanalyticsid",
+    FATHOM_ANALYTICS_ID: "TPQFBBUO",
   },
   images: {
     deviceSizes: [400, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,25 +4,7 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head>
-          {/* Global Site Tag (gtag.js) - Google Analytics */}
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${process.env.GOOGLE_ANALYTICS_ID}', {
-                  page_path: window.location.pathname,
-                });
-            `,
-            }}
-          />
-        </Head>
+        <Head></Head>
         <body>
           <Main />
           <NextScript />
