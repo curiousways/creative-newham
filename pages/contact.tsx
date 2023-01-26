1056511605e567da44df8698e0aa934162b45998
Oluwafemi Kayode <mrlapinho@gmail.com>
2023-01-26 16:38:10 +0000

    SEO for home, about, contact and join us page

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,37 +1,50 @@
 import type { NextPage } from "next";
+
+import { NextSeo } from "next-seo";
+
 import Twitter from "@/components/Twitter";
 import Linkedin from "@/components/Linkedin";
 import Email from "@/components/Email";
-import Panel from "@/components/panel/Panel";
+import Panel from "@/components/Panel/Panel";
 import SocialLink from "@/components/SocialLink";
 
 const Contact: NextPage = () => {
   return (
-    <main className="container">
-      <Panel title="Contact">
-        <SocialLink
-          icon={Email}
-          handle="vicki@rosettaarts.org"
-          url="mailto:vicki@rosettaarts.org"
-        >
-          Email
-        </SocialLink>
-        <SocialLink
-          icon={Linkedin}
-          handle="Creative Newham"
-          url="https://linkedin.com/"
-        >
-          Linkedin
-        </SocialLink>
-        <SocialLink
-          icon={Twitter}
-          handle="@creativenewham"
-          url="https://www.twitter.com/creativenewham/"
-        >
-          Twitter
-        </SocialLink>
-      </Panel>
-    </main>
+    <>
+      <NextSeo
+        title="Contact - Contact Creative Newham"
+        description="Contact Creative Newham"
+        openGraph={{
+          description: "Contact Creative Newham",
+        }}
+      />
+
+      <main className="container">
+        <Panel title="Contact">
+          <SocialLink
+            icon={Email}
+            handle="vicki@rosettaarts.org"
+            url="mailto:vicki@rosettaarts.org"
+          >
+            Email
+          </SocialLink>
+          <SocialLink
+            icon={Linkedin}
+            handle="Creative Newham"
+            url="https://linkedin.com/"
+          >
+            Linkedin
+          </SocialLink>
+          <SocialLink
+            icon={Twitter}
+            handle="@creativenewham"
+            url="https://www.twitter.com/creativenewham/"
+          >
+            Twitter
+          </SocialLink>
+        </Panel>
+      </main>
+    </>
   );
 };
 
