16ee172ffdbc17d0b12053fe53ec4b039f926aec
alexandermendes <alexanderhmendes@gmail.com>
2025-02-09 09:59:18 +0000

    refactor: remove useless bracket

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -28,7 +28,7 @@ const navItems = [
 
 const Nav = () => {
   return (
-    (<motion.div
+    <motion.div
       initial={{ opacity: 0, y: -10 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.5 }}
@@ -51,8 +51,8 @@ const Nav = () => {
           ))}
         </ul>
       </div>
-    </motion.div>)
-  );
+    </motion.div>
+  )
 };
 
 export default Nav;
